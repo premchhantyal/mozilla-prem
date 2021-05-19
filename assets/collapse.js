c8ce5a142cc9096c7fbb1cfed0c595805412581c
prem111 <premchhantyal2010@gmail.com>
2021-05-18 23:34:11 -0400

    project start

--- assets/collapse.js
+++ assets/collapse.js
@@ -1,18 +1,12 @@
-(function() {
-	"use strict";
-
-	// Add Scripts Here
-	var itemDetail = document.getElementsByClassName("item-detail");
+$(document).ready(function(){
+    var itemDetail = document.getElementsByClassName("item-detail");
     itemDetail[0].addEventListener("click", function() {
         this.classList.toggle("item-detail-active");
         var content = (document.getElementsByClassName("detail-content"))[0];
         if (content.style.maxHeight){
           content.style.maxHeight = null;
-          content.style.padding = "0";
         } else {
           content.style.maxHeight = content.scrollHeight + "px";
-          content.style.padding = "10px";
         } 
       });
-    	
-})();
+})
